f6b6895eba1996575bf9b1b58736242081ba18a6
Jasmine <79547445+JasmineBautista@users.noreply.github.com>
2025-09-16 19:32:18 +1000

    Last checkpoint before trying react bootstrap ig

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "./components/navbar"; // Adjust path if needed
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return <Navbar />;
+};
+
+export default App;
---
+++ my-react-app/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Login</li>
+        <li>Search</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
