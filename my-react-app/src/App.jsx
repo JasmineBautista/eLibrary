28daa50bf25f081f5ef52bc8c946a7384cf27660
Jasmine <79547445+JasmineBautista@users.noreply.github.com>
2025-09-30 18:51:03 +1000

    something something

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar"; // Adjust path if needed
 import LandingPage from "./pages/LandingPage"; // Assuming LandingPage.jsx is in the same directory
 
